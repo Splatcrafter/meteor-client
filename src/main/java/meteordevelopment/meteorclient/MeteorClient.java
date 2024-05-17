fabfe6b174c1e4e3468b0b5f2c46f89826fd0db6
Erik <the.real.splatcrafter@gmail.com>
2024-05-17 15:30:40 +0200

    Add new features to Redstone Client fork
    Added package info for new Redstone Client, which is a modified version of the Meteor Client used for Minecraft gaming. The modifications are clearly marked in the code to comply with transparency required by the GNU General Public License v3.0. Moreover, queue for packet sending feature is introduced, ensuring not more than 5 movement packets are sent per tick. Introduce mixin for sending packets immediately. These changes optimize the Redstone Client performance in game cheats.

--- src/main/java/meteordevelopment/meteorclient/MeteorClient.java
+++ src/main/java/meteordevelopment/meteorclient/MeteorClient.java
@@ -14,6 +14,7 @@ import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.gui.GuiThemes;
 import meteordevelopment.meteorclient.gui.WidgetScreen;
 import meteordevelopment.meteorclient.gui.tabs.Tabs;
+import meteordevelopment.meteorclient.mixin.ClientConnectionInvoker;
 import meteordevelopment.meteorclient.systems.Systems;
 import meteordevelopment.meteorclient.systems.config.Config;
 import meteordevelopment.meteorclient.systems.modules.Categories;
@@ -36,11 +37,16 @@ import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.metadata.ModMetadata;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.lang.invoke.MethodHandles;
+import java.util.LinkedList;
 
 public class MeteorClient implements ClientModInitializer {
     public static final String MOD_ID = "meteor-client";
@@ -56,6 +62,13 @@ public class MeteorClient implements ClientModInitializer {
     public static final IEventBus EVENT_BUS = new EventBus();
     public static final File FOLDER = FabricLoader.getInstance().getGameDir().resolve(MOD_ID).toFile();
     public static final Logger LOG;
+
+    // Redstone Client start
+    public static ClientPlayNetworkHandler networkHandler;
+    public static long globalTimer = 0;
+    public static boolean networkActive = false;
+    public static final LinkedList<Packet<?>> packetQueue = new LinkedList<>();  // Max 5 per tick
+    // Redstone Client end
 
     static {
         MOD_META = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow().getMetadata();
@@ -183,4 +196,27 @@ public class MeteorClient implements ClientModInitializer {
 
         wasWidgetScreen = event.screen instanceof WidgetScreen;
     }
+
+    // Redstone Client start
+    public static void tickEnd(MinecraftClient client) {
+        // Update variables
+        if (client.player != null && !networkActive) {
+            networkActive = true;
+            networkHandler = client.getNetworkHandler();
+        } else if (client.player == null && networkActive) {
+            networkActive = false;
+        }
+        globalTimer++;
+
+        // Send packets from queue (max 5)
+        int movementPacketsLeft = 5;
+        while (!packetQueue.isEmpty() && movementPacketsLeft > 0) {
+            Packet<?> packet = packetQueue.removeFirst();
+            if (packet instanceof PlayerMoveC2SPacket || packet instanceof VehicleMoveC2SPacket) {
+                movementPacketsLeft--;
+            }
+            ((ClientConnectionInvoker) networkHandler.getConnection())._sendImmediately(packet, null, false);
+        }
+    }
+    // Redstone Client end
 }
---
+++ src/main/java/meteordevelopment/meteorclient/rd/package-info.java
@@ -0,0 +1,32 @@
+/**
+ * This package contains classes from the "Redstone Client".
+ * <p>
+ * The Redstone Client is a Minecraft mod that is a fork of the Meteor Client.
+ * It is a mod that is used for cheating in Minecraft.
+ * </p>
+ * <p>
+ * It has been modified to include additional features and changes that are not present in the original Meteor Client.
+ * This distribution is not affiliated with the Meteor Development team and will never be.
+ * </p>
+ * <p>
+ * Modifications in the original Meteor Client MUST be visible in the source.
+ * Here is an example for comments where the Redstone Client modifies the Meteor Client:
+ * <pre>
+ *         // Redstone Client start
+ *         final boolean example = true;
+ *         // Redstone Client end
+ *         final int a = 1;
+ *         final int b = 2;
+ *         final int c = 3;
+ *
+ *         public void foo() {
+ *         // ...
+ *         }
+ * </p>
+ * <p>
+ *     With this example, it is clear where the Redstone Client modifies the Meteor Client.
+ *     This is important for transparency and to comply with the GNU General Public License v3.0.
+ *     The GNU General Public License v3.0 requires that the source code of the modified version is visible.
+ * </p>
+ */
+package meteordevelopment.meteorclient.rd;
